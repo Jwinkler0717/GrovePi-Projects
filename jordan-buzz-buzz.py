64eb03201c320895c52fb536b072a113ee35271d
Jwinkler0717 <jwinkler0717@lsr7.net>
2017-02-23 13:25:38 -0600

    Made the buzzer faster

---
+++ jordan-buzz-buzz.py
@@ -0,0 +1,40 @@
+############################################################
+# Simple Python program that repeatedly beeps a
+# Grove Buzzer attached to port D2.
+#
+# To run this program from the bash command prompt,
+# type this and hit Enter:
+#
+#    sudo python jordan-buzz-buzz.py
+#
+############################################################
+
+import time
+import grovepi
+import sys
+
+# Actuator settings
+buzzer = 5                             # Buzzer connected to D2 port
+
+grovepi.pinMode(buzzer, "OUTPUT")
+
+while True:
+  
+  try:
+    grovepi.digitalWrite(buzzer, 1)    # buzzer on
+    print "buzzer on"
+    time.sleep(0.5)
+
+    grovepi.digitalWrite(buzzer, 0)    # buzzer off
+    print "buzzer off"
+    time.sleep(0.5)
+
+  except KeyboardInterrupt:
+    grovepi.digitalWrite(buzzer, 0)
+    break
+  except IOError:
+    print "IOError"
+  except:
+    print "Unexpected error, continuing"
+    print "sys.exc_info()[0]: ", sys.exc_info()[0]
+
